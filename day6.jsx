c3d71258700721230a75a737fa2319c7e61a1cdf
Gerald Briyolan <gerald.briyolan2002@gmail.com>
2024-07-19 09:25:41 +0530

    Add files via upload

---
+++ day6.jsx
@@ -0,0 +1,12 @@
+console.log("hi")
+const Name=()=>{
+    console.log("It's a react file")
+    return(
+        <div className="heading">
+            Gerald Briyolan B
+        </div>
+
+    )
+   
+} 
+<Name/>
